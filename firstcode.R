ab3dd55be2570af35ec56f2df519ffb2f89062fc
monalisasingh <31319969+monalisasingh@users.noreply.github.com>
2017-08-29 01:54:40 +0530

    first code

---
+++ firstcode.R
@@ -0,0 +1,41 @@
+install.packages("swirl")
+library(swirl)
+install_course("Course Name Here")
+swirl()
+swirl()
+library("swirl")
+swirl()
+print("hello")
+print("hello" , quote=False)
+print("hello" , quote=FALSE)
+print("hello","how","are","you?")
+print(paste("how","are", "you?")
+)
+x
+x [1]
+3 6 8
+x [1] 3 6 8
+x <- 3 6 8
+x <-3
+x <-6
+x<-8
+x
+x <- c(3,6,8)
+x
+y <- c(2,9,0)
+y
+x+y
+x+1
+x+a(1,4)
+a <- c(1,4)
+a
+x + a
+b <- c( 1,4,7,3,8,7,9,2)
+b
+sort(b)
+order(b)
+order(b)
+exit
+bye(r)
+0
+quit()
